68096257a4db9fda517015399e5baf234cfead22
Philippe TERRANOVA <pterranova@ippon.fr>
2022-07-27 18:59:39 +0200

    modify for aws

--- src/main/java/fr/ippon/testaws/HelloWorld.java
+++ src/main/java/fr/ippon/testaws/HelloWorld.java
@@ -1,6 +1,5 @@
 package fr.ippon.testaws;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +9,7 @@ import java.io.PrintWriter;
 public class HelloWorld extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
         out.println("Hello World !");
     }
