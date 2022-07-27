ee65ab887420ae0f81a50c9221184080ecdf566f
Philippe TERRANOVA <pterranova@ippon.fr>
2022-07-27 18:37:15 +0200

    initial commit

---
+++ src/main/java/fr/ippon/testaws/HelloWorld.java
@@ -0,0 +1,18 @@
+package fr.ippon.testaws;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Hello World !");
+    }
+
+}
